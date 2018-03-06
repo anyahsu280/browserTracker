98231ef512023f0d49e1f1a6ef27a7e16826f8c5
Anya Hsu <anyahsu280@gmail.com>
2018-03-05 23:32:28 -0800

    basic stuff

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // --- Send request example: ---
+  // var server = "http://localhost:8080";
+  // var data = JSON.stringify({
+  //     sites: sites,
+  //     etc...
+  // });
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("GET", server + "/getTopSites");
+  // xhr.send(data);
+}, false);
