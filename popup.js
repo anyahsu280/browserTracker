be5e35999eaf5e66b5c0f7744d2148f7df8643cd
Anya Hsu <anyahsu280@gmail.com>
2018-03-07 10:48:26 -0800

    server working

--- popup.js
+++ popup.js
@@ -1,11 +1,26 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-  // --- Send request example: ---
-  // var server = "http://localhost:8080";
-  // var data = JSON.stringify({
-  //     sites: sites,
-  //     etc...
-  // });
-  // var xhr = new XMLHttpRequest();
-  // xhr.open("GET", server + "/getTopSites");
-  // xhr.send(data);
+    // --- Send request example: ---
+    var server = "http://localhost:8080";
+
+    var testButton = document.getElementById('test');
+    var resetButton = document.getElementById('reset');
+    var response = document.getElementById('response');
+    testButton.addEventListener('click', function(event) {
+        var xhr = new XMLHttpRequest();
+        //using a hardcoded URL here, need to populate these parameters with values from popup's inputs
+        xhr.open("GET", server + "/topSites?sites=5&dateRange=1&currentDate=" + Date.now());
+        xhr.onload = function() {
+            response.innerHTML = xhr.response;
+        }
+        xhr.send();
+    });
+    resetButton.addEventListener('click', function(event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", server + "/reset");
+        xhr.onload = function() {
+            response.innerHTML = xhr.response;
+        }
+        xhr.send();
+    });
 }, false);
